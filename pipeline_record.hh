cb1c083d828c9fd79d93233d489e369bdaee1469
David Sheffield <sheffield.david@gmail.com>
2024-11-28 09:47:20 -0800

    update pipetrace library header

--- pipeline_record.hh
+++ pipeline_record.hh
@@ -1,9 +1,9 @@
 #ifndef __pipeline_record_hh__
 #define __pipeline_record_hh__
 
-//#if BOOST_VERSION >= 107400
+#if BOOST_VERSION >= 107400
 #include <boost/serialization/library_version_type.hpp>
-//#endif
+#endif
 
 
 #include <boost/serialization/serialization.hpp>
@@ -24,7 +24,10 @@ public:
   std::string disasm;
   uint64_t pc;
   uint64_t fetch_cycle, alloc_cycle, sched_cycle, complete_cycle, retire_cycle;
+  uint64_t p1_hit_cycle, p1_miss_cycle, l1d_replay;
   bool faulted;
+  
+  std::list<uint64_t> l1d_blocks, l1d_sd;  
   friend class boost::serialization::access;
   template<class Archive>
   void serialize(Archive & ar, const unsigned int version) {
@@ -36,7 +39,12 @@ public:
     ar & sched_cycle;
     ar & complete_cycle;
     ar & retire_cycle;
-    ar & faulted;
+    ar & p1_hit_cycle;
+    ar & p1_miss_cycle;
+    ar & l1d_replay;
+    ar & faulted;    
+    ar & l1d_blocks;
+    ar & l1d_sd;
   }
 public:
   pipeline_record(uint64_t uuid,
@@ -47,13 +55,22 @@ public:
 		  uint64_t sched_cycle,
 		  uint64_t complete_cycle,
 		  uint64_t retire_cycle,
+		  uint64_t p1_hit_cycle,
+		  uint64_t p1_miss_cycle,
+		  uint64_t l1d_replay,
+		  const std::list<uint64_t> &l1d_blocks,
+		  const std::list<uint64_t> &l1d_sd,
 		  bool faulted) :
     uuid(uuid), disasm(disasm), pc(pc), fetch_cycle(fetch_cycle), alloc_cycle(alloc_cycle),
     sched_cycle(sched_cycle), complete_cycle(complete_cycle), retire_cycle(retire_cycle),
-    faulted(faulted) {}
+    p1_hit_cycle(p1_hit_cycle), p1_miss_cycle(p1_miss_cycle), l1d_replay(l1d_replay),
+    faulted(faulted),
+    l1d_blocks(l1d_blocks), l1d_sd(l1d_sd)  {}
   pipeline_record() :
     uuid(~0UL), disasm(""), pc(0), fetch_cycle(0), alloc_cycle(0),
-    complete_cycle(0), retire_cycle(0), faulted(false) {}
+    complete_cycle(0), retire_cycle(0),
+    p1_hit_cycle(~0UL), p1_miss_cycle(~0UL), l1d_replay(~0UL),
+    faulted(false) {}
 
   friend std::ostream &operator<<(std::ostream &out, const pipeline_record &r) {
     out << r.disasm << "," << std::hex <<  r.pc << std::dec
@@ -114,6 +131,11 @@ public:
 	      uint64_t sched_cycle,
 	      uint64_t complete_cycle,
 	      uint64_t retire_cycle,
+	      uint64_t p1_hit_cycle,
+	      uint64_t p1_miss_cycle,
+	      uint64_t l1d_replay,
+	      const std::list<uint64_t> &l1d_blocks,
+	      const std::list<uint64_t> &l1d_sd,	      
 	      bool faulted) {
 
     assert(fetch_cycle != (~0UL));
@@ -131,6 +153,11 @@ public:
 			 sched_cycle,
 			 complete_cycle,
 			 retire_cycle,
+			 p1_hit_cycle,
+			 p1_miss_cycle,
+			 l1d_replay,
+			 l1d_blocks,
+			 l1d_sd,
 			 faulted);
   }
 };
